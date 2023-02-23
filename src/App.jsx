57c54136820414b26644e5f5bbbef1168b29a73a
talingo <llusa.lautaro@gmail.com>
2023-02-23 10:03:09 +0100

    routed app

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Welcome from "./pages/Welcome/Welcome"
 import Home from "./pages/Home/Home"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
-import CardCoche from "./components/card/CardCoche";
-import Card from "./components/card/Card";
-import ViewCard from "./components/card/ViewCard";
+import CardCoche from "./components/CarCard/CardCoche";
+import Card from "./components/CarCard/Card";
+import ViewCard from "./components/CarCard/ViewCard";
 
 
 function App() {
