6f3ef5b6cd79d79b5213ef3b4464e06c0dc82263
unknown <marielayamilex@yahoo.es>
2023-02-23 08:30:47 +0100

    update FormCar

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import './App.css';
 function App() {
   return (
     <>
-    <Routes>
-        <Route path="/" element={<Welcome />} />
-    </Routes>
+      <Routes>
+          <Route path="/" element={<Welcome />} />
+      </Routes>
     </>
   );
 }
