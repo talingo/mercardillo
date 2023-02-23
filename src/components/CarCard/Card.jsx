990005052f935337a887ab430a755656ea9c4161
carlilu <carluppi99@gmail.com>
2023-02-23 12:19:30 +0100

    localhost

--- src/components/CarCard/Card.jsx
+++ src/components/CarCard/Card.jsx
@@ -1,22 +1,46 @@
 import React from 'react'
 import "./card.scss";
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+const url = 'http://localhost:8000/api'
 
-const card = (props) => {
+const Card = () => {
+
+    const [elementos, setElementos] = useState([]);
+
+    useEffect( () => {
+        getAllElements();
+    }, [])
+
+    const getAllElements = async () => {
+        const response = await axios.get(`${url}/proyectos`);
+        setElementos(response.data);
+    }
+
+
   return (
     <div>
         <div className='cont1'>
             <div className='img'>
-                 <img src="https://gomore.imgix.net/uploads/car_picture/image/676941/car_7fb87d3f-7efc-4057-995f-4d6fb272e173.jpg?ixlib=rails-2.1.2&fit=crop&w=770&h=513&auto=format%2Ccompress" alt="" />
+                <img src="https://gomore.imgix.net/uploads/car_picture/image/676941/car_7fb87d3f-7efc-4057-995f-4d6fb272e173.jpg?ixlib=rails-2.1.2&fit=crop&w=770&h=513&auto=format%2Ccompress" alt="" />
                  {/* <img src={props.image} alt="" /> */}
             </div>
 
             <div className='text'> 
-                <h1>Mercedes</h1> 
-                <p>4 Asientos </p>
+
+
+            { elementos.map( (elemento) => (
+              <div key={elemento.id}>
+                <h1>{elemento.title}Mercedes</h1> 
+                <p>{elemento.subtitle}4 Asientos </p>
                 <h3>Precio $150 </h3>
-                <Link to="/cardid"><button>Alquilar </button></Link>
+                  <div>
+                    <Link to="/cardid"> <button>Alquilar </button>  </Link>
+                  </div>
+                </div>
+                ))
+                }
             </div>
 
         </div>
@@ -24,4 +48,4 @@ const card = (props) => {
   )
 }
 
-export default card
+export default Card
